21e7ac44b24031f0922ef19e476917488def66d6
Nebhk <Nebkellil@gmail.com>
2022-03-10 18:56:19 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - print the string in the put function
  *
- * * main - print the string in the put function
+ * Description: using the main function
  *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  *
  * * Return: 0
  *
- * */
+ */
 
 int main(void)
 
 {
-
+/*here the main function starts*/
 		char c;
 
 			int i;
