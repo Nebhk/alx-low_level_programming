4ec7fd2a3f6e9c4b18615e655deb9716267d1251
Nebhk <Nebkellil@gmail.com>
2022-03-15 13:18:19 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * checks if a character is in lowercare
+ *
+ * Return 1 if c is lowercare 
+ * 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
+
