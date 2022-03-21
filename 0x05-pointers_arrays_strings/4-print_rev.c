e00461d4ce434020b86a80a3412b6da9a0d16147
Nebhk <Nebkellil@gmail.com>
2022-03-21 13:26:12 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_rev - prints strings in a reverse order
- * @str: parameter
+ * @s: parameter
  */
 void print_rev(char *s)
 {
 	int i;
 	int j;
 
-	for(i = 0; s[i] != 0; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 	}
 	for (j = i - 1; j >= 0; j--)
